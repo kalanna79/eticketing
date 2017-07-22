8c362c2d538e23d9cd2ed8f9a5f4ed0f6f73cbbd
kalanna79 <natacha@boudetnature.com>
2017-07-22 18:47:49 +0200

    endline characters add

--- src/NbGraphics/CoreBundle/Controller/OrderController.php
+++ src/NbGraphics/CoreBundle/Controller/OrderController.php
@@ -1,1 +1,56 @@
-<?phpnamespace NbGraphics\CoreBundle\Controller;use Symfony\Bundle\FrameworkBundle\Controller\Controller;use Symfony\Component\HttpFoundation\Request;use NbGraphics\CoreBundle\Form\Type\NbTicketsType;class OrderController extends Controller{    public function nbticketsAction(Request $request)    {        $session = $request->getSession();        $nbBillets = array('NbBillets' => 'Nombre de billets');        $session->set('nombre',$nbBillets);        $form3 = $this->createForm(NbTicketsType::class);                if ($request->isMethod('POST'))        {            $form3->handleRequest($request);                        if ($form3->isValid())            {                $data = $form3->getData();                                if ($data['choiceNb'] == 1)                {                    $data['NbBillets'] = 1;                }                $session->set('nombre', $data['NbBillets']);                return $this->redirectToRoute('order');            }        }        return $this->render('NbGraphicsCoreBundle:Order:nbtickets.html.twig',  array(            'form3' => $form3->createView()));    }            public function orderAction(Request $request)    {            $session = $request->getSession();        $nbBillets = $session->get('nombre');        $basket = $this->get('nb_graphics_core.booking')->setOrder($nbBillets, $request);        if ($basket->isSubmitted() && $basket->isValid())        {            return $this->redirectToRoute('recap');        }                 return $this->render('NbGraphicsCoreBundle:Order:order.html.twig',  array(        'form' => $basket->createView()));    }} 
+<?php
+
+namespace NbGraphics\CoreBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use NbGraphics\CoreBundle\Form\Type\NbTicketsType;
+
+
+class OrderController extends Controller
+{
+    public function nbticketsAction(Request $request)
+    {
+        $session = $request->getSession();
+        $nbBillets = array('NbBillets' => 'Nombre de billets');
+        $session->set('nombre',$nbBillets);
+        $form3 = $this->createForm(NbTicketsType::class);
+        
+        if ($request->isMethod('POST'))
+        {
+            $form3->handleRequest($request);
+            
+            if ($form3->isValid())
+            {
+                $data = $form3->getData();
+                
+                if ($data['choiceNb'] == 1)
+                {
+                    $data['NbBillets'] = 1;
+                }
+                $session->set('nombre', $data['NbBillets']);
+                return $this->redirectToRoute('order');
+            }
+        }
+        return $this->render('NbGraphicsCoreBundle:Order:nbtickets.html.twig',  array(
+            'form3' => $form3->createView()));
+    }
+    
+    
+    public function orderAction(Request $request)
+    {
+    
+        $session = $request->getSession();
+        $nbBillets = $session->get('nombre');
+        $basket = $this->get('nb_graphics_core.booking')->setOrder($nbBillets, $request);
+        if ($basket->isSubmitted() && $basket->isValid())
+        {
+            return $this->redirectToRoute('recap');
+        }
+        
+         return $this->render('NbGraphicsCoreBundle:Order:order.html.twig',  array(
+        'form' => $basket->createView()));
+    }
+}
+ 
--- src/NbGraphics/CoreBundle/Validator/Constraints/IsDayValidValidator.php
+++ src/NbGraphics/CoreBundle/Validator/Constraints/IsDayValidValidator.php
@@ -1,1 +1,43 @@
-<?php    /**     * Created by PhpStorm.     * User: natacha     * Date: 09/07/2017     * Time: 15:15     */        namespace NbGraphics\CoreBundle\Validator\Constraints;    use Symfony\Component\Validator\Constraint;    use Symfony\Component\Validator\ConstraintValidator;    use Doctrine\ORM\EntityManagerInterface;        class IsDayValidValidator extends ConstraintValidator    {        private $holidays;                public function __construct(EntityManagerInterface $em, $holidays)        {            $this->em           = $em;            $this->holidays     = $holidays;        }            public function validate($visitdate, Constraint $constraint)        {            $today = new \DateTime();            $year = $today->format('Y');            $holidays = $this->holidays;                foreach ($holidays as $holiday) {                $holiday .= $year;                $holiday = date_create($holiday);                    if ($visitdate == $holiday) {                    $this->context->buildViolation($constraint->message)->addViolation();                }            }        }    }         
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: natacha
+     * Date: 09/07/2017
+     * Time: 15:15
+     */
+    
+    namespace NbGraphics\CoreBundle\Validator\Constraints;
+
+    use Symfony\Component\Validator\Constraint;
+    use Symfony\Component\Validator\ConstraintValidator;
+    use Doctrine\ORM\EntityManagerInterface;
+    
+
+    class IsDayValidValidator extends ConstraintValidator
+    {
+        private $holidays;
+        
+        public function __construct(EntityManagerInterface $em, $holidays)
+        {
+            $this->em           = $em;
+            $this->holidays     = $holidays;
+        }
+    
+        public function validate($visitdate, Constraint $constraint)
+        {
+            $today = new \DateTime();
+            $year = $today->format('Y');
+            $holidays = $this->holidays;
+    
+            foreach ($holidays as $holiday) {
+                $holiday .= $year;
+                $holiday = date_create($holiday);
+    
+                if ($visitdate == $holiday) {
+                    $this->context->buildViolation($constraint->message)->addViolation();
+                }
+            }
+        }
+    }
+    
+     
