38308a1131d5ead17f2cdbd23388ffc353678748
kalanna79 <natacha@boudetnature.com>
2017-07-22 23:35:02 +0200

    endline character format Unix

--- src/NbGraphics/CoreBundle/Controller/RecapController.php
+++ src/NbGraphics/CoreBundle/Controller/RecapController.php
@@ -1,1 +1,32 @@
-<?php    /**     * Created by PhpStorm.     * User: natacha     * Date: 29/06/2017     * Time: 13:36     */        namespace NbGraphics\CoreBundle\Controller;            use Symfony\Bundle\FrameworkBundle\Controller\Controller;    use Symfony\Component\HttpFoundation\Request;        class RecapController extends Controller    {        public function recapAction(Request $request)        {            $session = $request->getSession();            $repository = $this                ->getDoctrine()                ->getManager()                ->getRepository('NbGraphicsCoreBundle:Basket');                        $order = $repository->find($session->get('panier'));            $amount = ($order->getTotal()*100);            $session->set('amount', $amount);                        return $this->render('NbGraphicsCoreBundle:Order:recap.html.twig', array('order' => $order));        }    }     
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: natacha
+     * Date: 29/06/2017
+     * Time: 13:36
+     */
+    
+    namespace NbGraphics\CoreBundle\Controller;
+    
+    
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use Symfony\Component\HttpFoundation\Request;
+    
+    class RecapController extends Controller
+    {
+        public function recapAction(Request $request)
+        {
+            $session = $request->getSession();
+            $repository = $this
+                ->getDoctrine()
+                ->getManager()
+                ->getRepository('NbGraphicsCoreBundle:Basket');
+            
+            $order = $repository->find($session->get('panier'));
+            $amount = ($order->getTotal()*100);
+            $session->set('amount', $amount);
+            
+            return $this->render('NbGraphicsCoreBundle:Order:recap.html.twig', array('order' => $order));
+        }
+    }
+     
--- src/NbGraphics/CoreBundle/Validator/Constraints/IsDayValid.php
+++ src/NbGraphics/CoreBundle/Validator/Constraints/IsDayValid.php
@@ -1,1 +1,29 @@
-<?php        /**     * Created by PhpStorm.     * User: natacha     * Date: 09/07/2017     * Time: 10:36     */        namespace NbGraphics\CoreBundle\Validator\Constraints;    use Symfony\Component\Validator\Constraint;    /**     * @Annotation     */        class IsDayValid extends Constraint    {     public $message = 'holiday';          public function validatedBy()     {         return 'nb_core_holidays';     }    }         
+<?php
+    
+    /**
+     * Created by PhpStorm.
+     * User: natacha
+     * Date: 09/07/2017
+     * Time: 10:36
+     */
+    
+    namespace NbGraphics\CoreBundle\Validator\Constraints;
+
+    use Symfony\Component\Validator\Constraint;
+
+
+    /**
+     * @Annotation
+     */
+    
+    class IsDayValid extends Constraint
+    {
+     public $message = 'holiday';
+     
+     public function validatedBy()
+     {
+         return 'nb_core_holidays';
+     }
+    }
+    
+     
